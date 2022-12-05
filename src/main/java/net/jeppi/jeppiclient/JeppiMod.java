6b8dc10e2e0e7d5e3189d55d7c2e0b5332f0cfa5
Jeppi <jesper.fuella2@gmail.com>
2022-12-05 15:08:59 +0100

    Initial commit

---
+++ src/main/java/net/jeppi/jeppiclient/JeppiMod.java
@@ -0,0 +1,14 @@
+package net.jeppi.jeppiclient;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JeppiMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("JeppiClient");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jeppi/jeppiclient/JeppiModClient.java
@@ -0,0 +1,10 @@
+package net.jeppi.jeppiclient;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JeppiModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
